df113d07ab4e070fa48f405fcd5cd080ee7a5431
Arthur Vimond <arthurvimond@gmail.com>
2018-07-21 16:19:44 +0200

    feat(sample): implement ViewModel and DataBinding

--- sample/src/main/java/fr/arthurvimond/rxbindingadapters/sample/MainActivity.kt
+++ sample/src/main/java/fr/arthurvimond/rxbindingadapters/sample/MainActivity.kt
@@ -1,12 +1,28 @@
 package fr.arthurvimond.rxbindingadapters.sample
 
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import fr.arthurvimond.rxbindingadapters.sample.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
+    // UI
+    private val binding: MainActivityBinding by lazy {
+        DataBindingUtil.setContentView<MainActivityBinding>(this, R.layout.activity_main)
+    }
+
+    // Data
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProviders.of(this).get(MainViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Set ViewModel
+        binding.viewModel = viewModel
+
     }
 }
---
+++ sample/src/main/java/fr/arthurvimond/rxbindingadapters/sample/MainViewModel.kt
@@ -0,0 +1,13 @@
+package fr.arthurvimond.rxbindingadapters.sample
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+
+/**
+ * Created by Arthur Vimond on 21/07/2018.
+ */
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+
+
+}
